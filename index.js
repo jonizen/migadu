56dd7170c13de987734bf4a7e9d3c1a65dc3876e
Jonathan Stendahl <jonathan.stendahl@live.se>
2018-06-07 19:57:20 +0200

    Removed override of index.js and updated project.json

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-module.exports = Franz => class Migadu extends Franz {
-    async validateUrl(url) {
-      try {
-        const resp = await window.fetch(url, {
-          method: 'GET',
-        });
-        return resp.status === 200;
-      } catch (err) {
-        console.error(err);
-      }
-  
-      return false;
-    }
-  };
-  
+module.exports = Franz => Franz;
